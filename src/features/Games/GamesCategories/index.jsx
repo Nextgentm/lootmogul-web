ba4ce134ced8d770e2915f76115fd43fc9ad1a2e
Nilesh-sukalikar <nilesh.sukalikar@nextgentm.com>
2022-11-29 16:37:13 +0530

    adding search in games section

--- src/features/Games/GamesCategories/index.jsx
+++ src/features/Games/GamesCategories/index.jsx
@@ -1,82 +1,119 @@
 import { ScrollMenu } from "react-horizontal-scrolling-menu";
 import {
-  Box,
-  Text,
-  Flex,
-  Wrap,
-  WrapItem,
-  useMediaQuery,
-  Heading,
+    Box,
+    Text,
+    Flex,
+    Wrap,
+    WrapItem,
+    useMediaQuery,
+    Heading
 } from "@chakra-ui/react";
 import GamesCard from "../../InfluencerDetails/InfluencerGames/InfluencerGameCard";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import ViewAllBtn from "../../../components/ViewAllBtn";
 import {
-  LeftArrow,
-  RightArrow,
+    LeftArrow,
+    RightArrow
 } from "../../../components/ContentNavigator/arrows";
+import Search from "../../Influencers/components/Search";
+import structuredClone from "@ungap/structured-clone";
+
 const GamesCategories = ({ isMobileDevice, section }) => {
-  const ref = useRef();
-  const [isLargerScreen] = useMediaQuery("(min-width: 2200px)");
-  const arrowTrashhold = isMobileDevice ? 2 :isLargerScreen ? 7: 5;
-  const [showAll, setShowAll] = useState(false);
-  return (
-    <Box>
-      <Flex justify="space-between" my="40px" align="center" w="100%">
-      <Heading variant="sectionTitle">
-      {section.name}
-      </Heading>
-            {section?.contestmasters?.data.length > arrowTrashhold ? (
-              <Box 
-              display={{
-                base: "none",
-                md: "flex",
-              }}
-                onClick={() => setShowAll(!showAll)}
-                pos="relative"
-                right="0"
-              >
-                <ViewAllBtn />
-              </Box>
+    const ref = useRef();
+    const [isLargerScreen] = useMediaQuery("(min-width: 2200px)");
+    const arrowTrashhold = isMobileDevice ? 2 : isLargerScreen ? 7 : 5;
+    const [showAll, setShowAll] = useState(false);
+    const [searchText, setSearchText] = useState("");
+    const [content, setContent] = useState({});
+    const [contentBackUp, setContentBackUp] = useState({});
+
+    useEffect(() => {
+        if (searchText.length > 3) {
+            const clonedData = structuredClone(content);
+            clonedData.contestmasters.data =
+                content?.contestmasters?.data.filter((x) =>
+                    x.name.toLowerCase().includes(searchText.toLowerCase())
+                );
+            setContent(clonedData);
+        } else {
+            console.log(contentBackUp);
+            setContent(contentBackUp);
+        }
+    }, [searchText]);
+
+    useEffect(() => {
+        setContent(section);
+        setContentBackUp(section);
+    }, [section]);
+
+    return (
+        <Box>
+            <Flex justify="space-between" my="40px" align="center" w="100%">
+                <Heading variant="sectionTitle">{content.name}</Heading>
+                {content.name !== "Trending Tournament" ? (
+                    <Box width={"250px"} right="0">
+                        <Search searchText={setSearchText}></Search>
+                    </Box>
+                ) : (
+                    ""
+                )}
+                {content?.contestmasters?.data.length > arrowTrashhold ? (
+                    <Box
+                        display={{
+                            base: "none",
+                            md: "flex"
+                        }}
+                        onClick={() => setShowAll(!showAll)}
+                        pos="relative"
+                        right="0"
+                    >
+                        <ViewAllBtn />
+                    </Box>
+                ) : (
+                    ""
+                )}
+            </Flex>
+            {showAll ? (
+                <Wrap m="auto !important">
+                    {content?.contestmasters?.data
+                        .sort((a, b) => a.priority - b.priority)
+                        .map((cm, index) => (
+                            <WrapItem m="auto !important" mb="2%!important">
+                                <GamesCard
+                                    style={{ mr: "24px" }}
+                                    key={`gamescard-${index}`}
+                                    contestmaster={cm}
+                                    sectionName={content.name}
+                                />
+                            </WrapItem>
+                        ))}
+                </Wrap>
             ) : (
-              ""
+                <Box mx={[2.5, 0]}>
+                    <ScrollMenu
+                        LeftArrow={LeftArrow}
+                        RightArrow={RightArrow}
+                        apiRef={ref}
+                    >
+                        {content?.contestmasters?.data
+                            ?.sort((a, b) => a.priority - b.priority)
+                            .map((cm, index) => (
+                                <GamesCard
+                                    style={{
+                                        w: ["75vw", "75vw", "370px", "370px"],
+                                        mx: 3
+                                    }}
+                                    itemId={`item-${index}`}
+                                    key={`item-${index}`}
+                                    contestmaster={cm}
+                                    sectionName={content.name}
+                                />
+                            ))}
+                    </ScrollMenu>
+                </Box>
             )}
-          
-      </Flex>
-      {showAll ? (
-        <Wrap m="auto !important">
-          {section?.contestmasters?.data
-            .sort((a, b) => a.priority - b.priority)
-            .map((cm, index) => (
-              <WrapItem m="auto !important" mb="2%!important">
-                <GamesCard
-                  style={{ mr: "24px" }}
-                  key={`gamescard-${index}`}
-                  contestmaster={cm}
-                  sectionName={section.name}
-                />
-              </WrapItem>
-            ))}
-        </Wrap>
-      ) : (
-        <Box mx={[2.5,0]}>
-        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} apiRef={ref} >
-          {section?.contestmasters?.data
-            ?.sort((a, b) => a.priority - b.priority)
-            .map((cm, index) => (
-              <GamesCard
-                style={{ w:["75vw","75vw","370px","370px"], mx:3}}
-                itemId={`item-${index}`}
-                key={`item-${index}`}
-                contestmaster={cm}
-                sectionName={section.name}
-              />
-            ))}
-        </ScrollMenu>
         </Box>
-      )}
-    </Box>
-  );
+    );
 };
 
 export default GamesCategories;
