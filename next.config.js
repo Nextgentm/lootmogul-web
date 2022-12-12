8a39ac99c69e2d294137e44169e99c167705867c
nilesh-sukalikar <nilesh.sukalikar@nextgentm.com>
2022-12-12 12:14:43 +0530

    env variable fixes and sentry integratio

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports =withBundleAnalyzer( {
+const withPlugins = require("next-compose-plugins");
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
+  sentry: {
+    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+    // for client-side builds. (This will be the default starting in
+    // `@sentry/nextjs` version 8.0.0.) See
+    // https://webpack.js.org/configuration/devtool/ and
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+    // for more information.
+    hideSourceMaps: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["media-content.lootmogul.com","1.bp.blogspot.com","drive.google.com","lootmogalimages.s3.ap-south-1.amazonaws.com","s3-qunami.s3-us-west-2.amazonaws.com","gamification.tpix.in","localhost",
@@ -1764,6 +1776,24 @@ module.exports =withBundleAnalyzer( {
        }
       
     ]
-  },
-  
-});
+  }
+};
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// module.exports = withPlugins([withBundleAnalyzer], 
+//   withSentryConfig(moduleExports, sentryWebpackPluginOptions));
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+
+// module.exports =withBundleAnalyzer( {// });
---
+++ sentry.client.config.js
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN || 'https://12600968317b46e8ae982ccfdced50ab@o4504293426397184.ingest.sentry.io/4504293427904512',
+  tracesSampleRate: 1.0,
+  environment: "staging",
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
