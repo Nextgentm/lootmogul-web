3b0ccb33013e6d4c3933596dc70b35cd8c5d934d
nilesh-sukalikar <nilesh.sukalikar@nextgentm.com>
2022-12-27 21:28:23 +0530

    merging chnages

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://lootmogul.com',
-    generateRobotsTxt: process.env.NEXT_PUBLIC_SENTRY_ENV === 'production' ? true : false, // (optional)
+    generateRobotsTxt: NEXT_PUBLIC_SENTRY_ENV === 'production' ? true : false, // (optional)
     // ...other options
   }
   
