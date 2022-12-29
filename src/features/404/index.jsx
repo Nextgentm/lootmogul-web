46c86fbc1fd8ff3cdb43517ed49749c12645e2a8
vishal-lootmogule <vishal.solanki@nextgentm.com>
2022-12-29 18:51:33 +0530

    404 Changes for Button
    404 Changes for Button

--- src/features/404/index.jsx
+++ src/features/404/index.jsx
@@ -2,25 +2,30 @@ import { Box, Text, Heading } from "@chakra-ui/layout";
 import { Image,Button,Link } from '@chakra-ui/react';
 import React, {  } from "react";
 
-const NotFound = () => {
+const NotFound = (isActivePage) => {
+    console.log(isActivePage);
+    let return_ur = '';
+    let return_name = '';
+    if(isActivePage.isNfts){
+        return_ur = '/nfts';
+        return_name = 'Back To NFTs';
+    }
+    else{
+        return_ur = '/';
+        return_name = 'Back To Home';
+    }
     return (
-        
         <Box width="99vw" pt={["5%", "2%"]} pl={["5%", "2%"]} height="auto" background={"transparent radial-gradient(closest-side at 70% 50%, #481A7F 0%, #180529 70%) 0% 0% no-repeat padding-box;"}>
             
-            <Box w={["200px","300px","350px", "350px"]} mt={["5%", "5%","1%","1%"]} ml={["15%", "15%","1%","1%"]}>
-                <Link href='/' _hover={["textDecoration:none"]}>
-                    <Image src="https://lootmogul-wp-cdn-buckets.s3.us-west-2.amazonaws.com/wp-content/uploads/2022/12/20034811/LootMogul-Logo-2.png" alt="Dan Abramov" />
-                </Link>
-            </Box>
             <Box  w={["100%","100%","100%","50%"]} p={4} color='white' display={"inline-block"} verticalAlign="top" pt={["5%","5%", "175px"]} pl={["5%", "2%"]}>
                 <Heading variant="siteHeading" color="#fff" fontWeight={500} fontSize={["80","80","80","125"]} textAlign={['center','center','center','left']}>
                 404 ERROR
                 </Heading>
                 <Text style={{ lineHeight: '25px' }} w={["100%","100%","100%", "85%"]} mt={["0%", "5%"]} ml={["0%", "0%"]} variant="hint" color={"#fff"} fontSize={18} textAlign={['center','center','center','left']} >
                 The page you requested could not be found</Text>
-                <Link href='/nfts' _hover={["textDecoration:none"]}>
+                <Link href={return_ur} _hover={["textDecoration:none"]}>
                     <Button variant="solid" display={['flex','flex','flex','inline-flex']} margin={['auto']} mt={["30px", "30px"]}>
-                        Back To Home
+                       {return_name}
                     </Button>
                 </Link>
             </Box>
