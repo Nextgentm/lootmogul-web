df0b4c6d39b1c2a537ac33104d7ad647eb5d5e9c
Parth Gadhiya <parth.gadhiya@nextgentm.com>
2023-02-23 18:04:54 +0530

    style

--- src/components/LMModal/DepositWithdraw/withdrawStop.jsx
+++ src/components/LMModal/DepositWithdraw/withdrawStop.jsx
@@ -17,7 +17,7 @@ const WalletHeader = ({ totalAmount, isDeposit, winAmount }) => {
                     <Flex width={["100%", "100%"]}>
                         <Box
                             width="100%"
-                            paddingTop="1%"
+                            paddingTop="5%"
                             borderTopLeftRadius="8px"
                             paddingBottom="0%"
                             paddingLeft={"2%"}
@@ -28,7 +28,7 @@ const WalletHeader = ({ totalAmount, isDeposit, winAmount }) => {
                                 mt="10px"
                                 fontWeight="600"
                                 variant="modalHeader"
-                                fontSize={["12px","12px", "36px"]}
+                                fontSize={["22px","22px", "48px"]}
                                 fontFamily={"blanch"}
                             >
                                 {isDeposit ? "DEPOSIT" : "WITHDRAW"}
@@ -63,55 +63,6 @@ const WalletHeader = ({ totalAmount, isDeposit, winAmount }) => {
     );
 };
 
-const WalletSubHeader = ({ totalAmount, isDeposit, winAmount }) => {
-    const currentSize = useBreakpointValue({
-        base: "base",
-        sm: "sm",
-        md: "md"
-    });
-
-    return (
-        <Flex width="100%" bg="#1d052b"  borderBottom="0">
-            <Flex w="100%">
-                    <Box m="auto" justifyContent="center">                        
-                        <Heading
-                            fontWeight="400"
-                            fontFamily="Blanch"
-                            fontSize={["20px", "20px", "31px"]}
-                            ml={["6px!important", "15px!important"]}
-                            m="auto"
-                            color="white"
-                            textAlign="center"
-                            display="inline-flex"
-                        >
-                            {isDeposit ? "WALLET BALANCE IN CHIPS" : "WIN BALANCE IN CHIPS"}
-                        </Heading>
-                        <Image
-                            alt="tag"
-                            boxSize={["15px","20px","25px"]}
-                            src="/assets/Icon.png"
-                            ml="5px"
-                            display="inline-flex"
-                            verticalAlign="sub"
-                        />
-                        <Heading
-                        fontSize={["24px", "28px", "38px"]}
-                        mt="auto"
-                        ml="10px"
-                        mb="auto"
-                        color="primary"
-                        display="inline-flex"
-                    >
-                        {isDeposit ? totalAmount : winAmount}
-                    </Heading>
-                    </Box>
-
-                    
-                </Flex>
-        </Flex>  
-    );
-};
-
 const WalletFooter = () => {
     return (
         <Flex m="auto" width="100%" justifyContent="center">
@@ -138,7 +89,7 @@ const DepostWithdrawstop = ({ totalAmount, isDeposit, winAmount }) => {
                 totalAmount={totalAmount}
                 winAmount={winAmount}
             /> */}
-            <Text style={{color:'white', textAlign:'center', padding:'10px',fontWeight:700}}>We have paused our withdrawal till 28th Feb 2023</Text> 
+            <Text style={{color:'white', textAlign:'center', padding:'10%',paddingBottom:'23%',paddingTop:'23%',fontWeight:700,fontSize:'20px'}}>We have paused our withdrawal till 28th Feb 2023</Text> 
             <WalletFooter />
         </Box>
     );
