49f74c5d03166542b8cbe1daf4e8c6e85fa4229c
vishal-lootmogule <vishal.solanki@nextgentm.com>
2023-02-24 10:22:03 +0530

    Bugs
    Bugs

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -475,7 +475,7 @@ const Header = () => {
                     zIndex={"101"}
                     backgroundImage="/assets/bg-bar.png"
                     backgroundSize="cover"
-                    display={['block','block','flex','flex']}
+                    display={['block','block','flex','flex','flex']}
                 >
                     <Flex
                         w={isMobileDevice ? "100%" : "20%"}
