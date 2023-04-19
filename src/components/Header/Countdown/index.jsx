66e9ae0aa01f1cad1cdb61510f1c20ec5a4aed28
vishal-lootmogule <vishal.solanki@nextgentm.com>
2023-04-19 11:52:35 +0530

    Format Changes
    Format Changes

--- src/components/Header/Countdown/index.jsx
+++ src/components/Header/Countdown/index.jsx
@@ -9,21 +9,41 @@ function MyTimer({ expiryTimestamp }) {
       hours,
       days,
     } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+    const leftseconds = seconds >= 10 ? seconds.toString()[0] : '0';
+    const rightseconds = seconds >= 10 ? seconds.toString()[1] : seconds.toString();
+
+    const leftminutes = minutes >= 10 ? minutes.toString()[0] : '0';
+    const rightminutes = minutes >= 10 ? minutes.toString()[1] : minutes.toString();
+
+    const lefthours = hours >= 10 ? hours.toString()[0] : '0';
+    const righthours = hours >= 10 ? hours.toString()[1] : hours.toString();
+
+    const leftdays = days >= 10 ? days.toString()[0] : '0';
+    const rightdays = days >= 10 ? days.toString()[1] : days.toString();
+    
     return (
         
         <div style={{textAlign: 'center'}}>
             <Grid templateColumns='repeat(4, 1fr)' gap={1}>
-                <GridItem w='100%' h='50' p="5px 10px" bg='#F60C67' color="#fff" verticalAlign="middle" display="inline-grid" borderRadius="5px" fontSize="18px"><Text>{days}</Text><Text fontFamily="var(--chakra-fonts-Blanch)" lineHeight="0">Days</Text></GridItem>
-                <GridItem w='100%' h='50' p="5px 10px" bg='#F60C67' color="#fff" verticalAlign="middle" display="inline-grid" borderRadius="5px" fontSize="18px"><Text>{hours}</Text><Text fontFamily="var(--chakra-fonts-Blanch)" lineHeight="0">Hours</Text></GridItem>
-                <GridItem w='100%' h='50' p="5px 10px" bg='#F60C67' color="#fff" verticalAlign="middle" display="inline-grid" borderRadius="5px" fontSize="18px"><Text>{minutes}</Text><Text fontFamily="var(--chakra-fonts-Blanch)" lineHeight="0">Minutes</Text></GridItem>
-                <GridItem w='100%' h='50' p="5px 10px" bg='#F60C67' color="#fff" verticalAlign="middle" display="inline-grid" borderRadius="5px" fontSize="18px"><Text>{seconds}</Text><Text fontFamily="var(--chakra-fonts-Blanch)" lineHeight="0">Seconds</Text></GridItem>
+                <GridItem w='100%' h='50' p="5px 10px" bg='#F60C67' color="#fff" verticalAlign="middle" display="inline-grid" borderRadius="5px" fontSize="18px">
+                  <Text>{leftdays}{rightdays}</Text>
+                  <Text fontFamily="var(--chakra-fonts-Blanch)" lineHeight="0">Days</Text></GridItem>
+                <GridItem w='100%' h='50' p="5px 10px" bg='#F60C67' color="#fff" verticalAlign="middle" display="inline-grid" borderRadius="5px" fontSize="18px">
+                  <Text>{lefthours}{righthours}</Text>
+                  <Text fontFamily="var(--chakra-fonts-Blanch)" lineHeight="0">Hours</Text></GridItem>
+                <GridItem w='100%' h='50' p="5px 10px" bg='#F60C67' color="#fff" verticalAlign="middle" display="inline-grid" borderRadius="5px" fontSize="18px">
+                  <Text>{leftminutes}{rightseconds}</Text>
+                  <Text fontFamily="var(--chakra-fonts-Blanch)" lineHeight="0">Minutes</Text></GridItem>
+                <GridItem w='100%' h='50' p="5px 10px" bg='#F60C67' color="#fff" verticalAlign="middle" display="inline-grid" borderRadius="5px" fontSize="18px">
+                  <Text>{leftseconds}{rightseconds}</Text>
+                  <Text fontFamily="var(--chakra-fonts-Blanch)" lineHeight="0">Seconds</Text></GridItem>
             </Grid>    
         </div>
       );
     }
 const CountDown = ({ isOpen, onClose, renderMobileRoutes }) => {
     
-    const time = new Date('04-23-2023');
+    const time = new Date('04-24-2023 11:59');
     time.setSeconds(time.getSeconds()); // 10 minutes timer
     return (
       <div>
