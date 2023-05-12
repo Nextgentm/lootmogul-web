6257287f8a185a05c556f7c6b914760145cba2e2
vishal-lootmogule <vishal.solanki@nextgentm.com>
2023-05-12 13:34:01 +0530

    Remove Cookie bar
    Remove Cookie bar

--- pages/nfts/index.jsx
+++ pages/nfts/index.jsx
@@ -3,7 +3,6 @@ import dynamic from "next/dynamic";
 import MyPageLoader from "../../src/components/MyPageLoader";
 import SEOContainer from "../../src/features/SEOContainer";
 import { getSeoData } from "../../src/queries/strapiQueries";
-import CookieConsent from "react-cookie-consent";
 
 const defaultSEOData = {
     metaTitle:
@@ -21,18 +20,7 @@ const CollectiblesComponent = dynamic(
 export default function ColleNFTctiblePage({ nftCollections, seoData }) {
     return (
         <>
-        <CookieConsent
-            location="bottom"
-            buttonText="Accept"
-            cookieName="myAwesomeCookieName2"
-            style={{ background: "#100026", fontSize: "45px",fontFamily:"Blanch",lineHeight:"50px" }}
-            buttonStyle={{ color:"#fff", background: "#e90a63", fontSize: "25px",lineHeight:"1", width:"100px" }}
-            expires={150}
-            >
-            We value your privacy
-            {" "}
-            <br/><span style={{ fontSize: "25px",width:"75%",display:"block",lineHeight:"25px" }}>We use cookies on our website to give you the most relevant experience by remembering your preferences and repeat visits. By clicking “Accept”, you consent to the use of ALL the cookies.</span>
-        </CookieConsent>
+        
             <SEOContainer
                 seoData={seoData ? seoData[0]?.sharedSeo : defaultSEOData}
             />
