bcb0abe77db477d8a8f407feb576f2c6a0db28d9
sarthakkumar766 <sarthakkumar766@gmail.com>
2023-08-04 15:31:23 +0530

    fix utm_term mobbups

--- src/services/mobuppsCallService.js
+++ src/services/mobuppsCallService.js
@@ -3,19 +3,21 @@ import axios from "axios";
 
 const mobuppsCallService = async () => {
     const utm_medium = localStorage.getItem("utm_medium");
+    const utm_term = localStorage.getItem("utm_term");
 
-    console.log("*****************mobuppsCallService********************");
+    // console.log("*****************mobuppsCallService********************");
 
     let wmadv = null;
     if (utm_medium == "mobupps") {
+        console.log("*****************mobuppsCallService********************");
         wmadv = await axios.get(
-            `http://wmadv.go2cloud.org/aff_goal?a=lsr&goal_name=Gameplay&adv_id=5679&transaction_id=%7Bp1%7D`
+            `http://wmadv.go2cloud.org/aff_goal?a=lsr&goal_name=Gameplay&adv_id=5679&transaction_id=${utm_term}`
         );
     }
 
     const myMessage = {
-        message: wmadv,
-        wmadvUrl: `http://wmadv.go2cloud.org/aff_goal?a=lsr&goal_name=Gameplay&adv_id=5679&transaction_id=%7Bp1%7D`
+        message: "wmadv",
+        wmadvUrl: `http://wmadv.go2cloud.org/aff_goal?a=lsr&goal_name=Gameplay&adv_id=5679&transaction_id=${utm_term}`
     };
 
     Sentry.captureMessage(JSON.stringify(myMessage));
