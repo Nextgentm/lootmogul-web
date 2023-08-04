792ae6fb71ccbac2594649c72bb116405171566b
sarthakkumar766 <sarthakkumar766@gmail.com>
2023-08-04 16:21:44 +0530

    add https in get

--- src/services/mobuppsCallService.js
+++ src/services/mobuppsCallService.js
@@ -11,13 +11,13 @@ const mobuppsCallService = async () => {
     if (utm_medium == "mobupps") {
         console.log("*****************mobuppsCallService********************");
         wmadv = await axios.get(
-            `http://wmadv.go2cloud.org/aff_goal?a=lsr&goal_name=Gameplay&adv_id=5679&transaction_id=${utm_term}`
+            `https://wmadv.go2cloud.org/aff_goal?a=lsr&goal_name=Gameplay&adv_id=5679&transaction_id=${utm_term}`
         );
     }
 
     const myMessage = {
         message: "wmadv",
-        wmadvUrl: `http://wmadv.go2cloud.org/aff_goal?a=lsr&goal_name=Gameplay&adv_id=5679&transaction_id=${utm_term}`
+        wmadvUrl: `https://wmadv.go2cloud.org/aff_goal?a=lsr&goal_name=Gameplay&adv_id=5679&transaction_id=${utm_term}`
     };
 
     Sentry.captureMessage(JSON.stringify(myMessage));
