a862e2b2336e86f408cd8c2b5bc4f8cca694ef22
vishal-lootmogule <vishal.solanki@nextgentm.com>
2023-09-06 14:05:51 +0530

    Remove Console add code
    Remove Console add code

--- src/services/hasmindCallService.js
+++ src/services/hasmindCallService.js
@@ -6,16 +6,16 @@ const hasmindCallService = async () => {
     const utm_term = localStorage.getItem("logged_utm_term");
 
     // console.log("*****************hasmindCallService********************");
-    const myMessage1 = {
+    /*const myMessage1 = {
         message: "Start hashmind",
         wmadvUrl: `https://hasmindpvtltd10992690.o18.click/p?m=18338&event=FTD&tid=${utm_term}`
     };
 
-    Sentry.captureMessage(JSON.stringify(myMessage1));
+    Sentry.captureMessage(JSON.stringify(myMessage1));*/
 
     let wmadv = null;
     if (utm_medium == "hashmind") {
-        console.log("*****************hasmindCallService********************");
+        //console.log("*****************hasmindCallService********************");
         wmadv = await axios.get(
             `https://hasmindpvtltd10992690.o18.click/p?m=18338&event=FTD&tid=${utm_term}`
         );
