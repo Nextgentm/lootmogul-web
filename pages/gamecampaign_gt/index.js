f2b2c673a27aea5bdc9370d0526aac487b8ecf05
vishal-lootmogule <118430470+vishal-lootmogule@users.noreply.github.com>
2023-10-17 11:18:01 +0530

    New GT Duplicate Page
    New GT Duplicate Page

---
+++ pages/gamecampaign_gt/index.js
@@ -0,0 +1,115 @@
+import strapi from "../../src/utils/strapi";
+import SEOContainer from "../../src/features/SEOContainer";
+import { getSeoData } from "../../src/queries/strapiQueries";
+import GamesComponent from "../../src/features/Games/index";
+import MultipleLoggedInUser from "../../src/components/MultipleLoggedInUser";
+
+import Banner from "../../src/components/Web3Games/Banner";
+import TradingGame from "../../src/components/Web3Games/TradingGame";
+import BlockChainGame from "../../src/components/Web3Games/BlockChainGame";
+
+const defaultSEOData = {
+    metaTitle:"Lootmogul | Join LootMogul Web3 Sports Gaming",
+    metaDescription:"Immerse yourself in LootMogul's captivating blockchain games, where you'll not only earn valuable in-game rewards but also unlock real-world benefits!",
+    canonicalURL:process.env.NEXT_PUBLIC_SITE_URL+"/web3-games"
+};
+
+export default function GamesPage({
+  data,
+  banners = [],
+  contestSectionsData,
+  campaignsSectionsResData,
+  seoData,
+}) {
+  //console.log(data);
+  return (
+    <>
+      
+      <SEOContainer seoData={seoData?seoData[0]?.sharedSeo:defaultSEOData}/> 
+        <Banner bannerData={campaignsSectionsResData?.data[0] || []}/>
+        <TradingGame tradingCardData={campaignsSectionsResData?.data[0] || []}/>
+        <BlockChainGame 
+            contestSectionsData={contestSectionsData?.data || []}
+            contestmasters={data || []}
+            blockChainCardData={campaignsSectionsResData?.data[0] || []}
+        />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  // Fetch data from external API
+  let pageNo = 1;
+  let pageCount = 1;
+  let data = [];
+  do {
+    const res = await strapi.find("contestmasters", {
+      fields: ["name", "slug", "priority", "entryFee", "isFeatured", "retries"],
+      sort: "priority",
+      populate: {
+        contest_section: {
+          fields: ["name", "slug"],
+        },
+        icon: {
+          fields: ["name", "url"],
+        },
+        feeWallet: {
+          populate: {
+            currency: {
+              fields: ["type"],
+            },
+          },
+        },
+        reward: {},
+        game:{
+          fields:"*"
+        }
+      },
+
+      pagination: {
+        page: pageNo,
+        pageSize: 25,
+      },
+    });
+    if (res?.meta) {
+      data.push(res.data);
+      if (pageCount == 1) {
+        pageCount = res.meta.pagination.pageCount;
+      }
+    }
+    pageNo++;
+  } while (pageNo <= pageCount);
+  // Pass data to the page via props
+  data = data.flat();
+
+  try {
+    const contestSectionsRes = await fetch(
+      process.env.NEXT_PUBLIC_STRAPI_API_URL +
+        "/api/contest-sections?populate=image&sort=priority"
+    );
+
+    const campaignsSectionsRes = await fetch(
+      process.env.NEXT_PUBLIC_STRAPI_API_URL +
+        "/api/game-campaigns?populate=*"
+    );
+    const contestSectionsData = await contestSectionsRes.json();
+    const campaignsSectionsResData = await campaignsSectionsRes.json();
+
+    const bannersRes = await fetch(
+      process.env.NEXT_PUBLIC_STRAPI_API_URL +
+        "/api/campaigns?sort=priority&populate=bannerImage"
+    );
+
+    const banners = await bannersRes.json();
+    const seoData = await getSeoData("games");
+
+    return {
+      props: { data, contestSectionsData, campaignsSectionsResData, banners, seoData },
+      revalidate: 300,
+    };
+  } catch (error) {}
+  return {
+    props: { data },
+    revalidate: 300, // In seconds
+  };
+}
