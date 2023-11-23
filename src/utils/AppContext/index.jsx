45bed37567003e20b7f05d0dbee00466694e7b34
vishal-lootmogule <vishal.solanki@nextgentm.com>
2023-11-23 20:38:46 +0530

    Thankgaving_page
    Thankgaving_page

--- src/utils/AppContext/index.jsx
+++ src/utils/AppContext/index.jsx
@@ -1066,6 +1066,9 @@ export const AppContextContainer = ({ children }) => {
         if (router.route === "/gamecampaign") {
             router.push(redirectUrl);
         }
+        if (router.route === "/thanksgiving-campaign") {
+            router.push('/wallet');
+        }
         if (routePathAfterLogin) {
             if (routePathAfterLogin.nextPath === "/joining") {
                 CheckLocationAndConfirm(routePathAfterLogin.contestmaster);
