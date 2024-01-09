52b181bd3562c7ebdc6ac0064e36abce4e16f428
ssouvikk <souvik.edu.mondal@gmail.com>
2024-01-09 16:57:42 +0530

    location is getting stored in db if allowed

--- src/utils/AppContext/index.jsx
+++ src/utils/AppContext/index.jsx
@@ -573,9 +573,12 @@ export const AppContextContainer = ({ children }) => {
         if (data?.user) {
             window.localStorage.setItem("token", data.jwt);
 
-            getCurremtLocation().then((res) => {
+            getCurremtLocation().then(/* async */(res) => {
                 window.localStorage.setItem("lm_user_location", res?.country);
                 window.localStorage.setItem("lm_user_state", res?.state);
+                /* const updatedSession = await */strapi.request('PATCH', '/sessions/location', 
+                    { data : { state: res?.state, browserCountry: res?.country }}
+                )
             });
 
             if (data.user.is_new) {
@@ -807,6 +810,13 @@ export const AppContextContainer = ({ children }) => {
                         password: formData.password
                     };
                     data = await strapi.register(apiValues);
+                    getCurremtLocation().then((res) => {
+                        window.localStorage.setItem("lm_user_location", res?.country);
+                        window.localStorage.setItem("lm_user_state", res?.state);
+                        strapi.request('PATCH', '/sessions/location', 
+                            { data : { state: res?.state, browserCountry: res?.country }}
+                        )
+                    });
                 } catch ({ error }) {
                     toast({
                         title: error.message,
@@ -827,18 +837,13 @@ export const AppContextContainer = ({ children }) => {
                         password: formData.password
                     };
                     data = await strapi.login(apiValues);
-                    
                     setJwt(data.jwt);
-                    getCurremtLocation().then((res) => {
-                        window.localStorage.setItem(
-                            "lm_user_location",
-                            res?.country
-                        );
-
-                        window.localStorage.setItem(
-                            "lm_user_state",
-                            res?.state
-                        );
+                    getCurremtLocation().then(/* async */(res) => {
+                        window.localStorage.setItem("lm_user_location", res?.country);
+                        window.localStorage.setItem("lm_user_state", res?.state);
+                        /* const updatedSession = await */strapi.request('PATCH', '/sessions/location', 
+                            { data : { state: res?.state, browserCountry: res?.country }}
+                        )
                     });
                 } catch ({ error }) {
                     toast({
