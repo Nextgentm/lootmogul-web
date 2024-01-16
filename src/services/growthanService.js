9568b338e4738f94be721827aab7f9854995d063
vishal-lootmogule <vishal.solanki@nextgentm.com>
2024-01-16 14:47:48 +0530

    Update growthanService.js

--- src/services/growthanService.js
+++ src/services/growthanService.js
@@ -4,6 +4,7 @@ const growThanCallService = async () => {
     const utm_medium = localStorage.getItem("logged_utm_medium");
     const utm_term = localStorage.getItem("logged_utm_term");
     console.log("*****************growThanCallService Before********************");
+    console.log(process.env.NEXT_PUBLIC_SENTRY_ENV);
     let wmadv = null;
     if (utm_medium == "GT" && process.env.NEXT_PUBLIC_SENTRY_ENV === 'staging') {
         console.log("*****************growThanCallService called********************");
