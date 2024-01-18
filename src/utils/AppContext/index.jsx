09f45e35865eed0bd7590dbeeb104083e435307a
ssouvikk <souvik.edu.mondal@gmail.com>
2024-01-18 17:56:19 +0530

    loader is added for login n register

--- src/utils/AppContext/index.jsx
+++ src/utils/AppContext/index.jsx
@@ -29,14 +29,11 @@ export const AppContextContainer = ({ children }) => {
     const [isLoginModalActive, setLoginModalActive] = useState(false);
     // const [refetch, setRefetch] = useState(false);
 
-    const [isForgotPasswordModalActive, setForgotPasswordModalActive] =
-        useState(false);
-    const [isCheckYourMailModalActive, setCheckYourMailModalActive] =
-        useState(false);
-    const [isChangePasswordModalActive, setChangePasswordModalActive] =
-        useState(false);
-    const [isPasswordChangedModalActive, setPasswordChangedModalActive] =
-        useState(false);
+    const [loggingIn, setLoggingIn] = useState(false)
+    const [isForgotPasswordModalActive, setForgotPasswordModalActive] = useState(false);
+    const [isCheckYourMailModalActive, setCheckYourMailModalActive] = useState(false);
+    const [isChangePasswordModalActive, setChangePasswordModalActive] = useState(false);
+    const [isPasswordChangedModalActive, setPasswordChangedModalActive] = useState(false);
     const [isMobileDevice, setMobileDevice] = useState(false);
 
     const [withdrawFetch, setWithdrawFetch] = useState(true);
@@ -73,9 +70,8 @@ export const AppContextContainer = ({ children }) => {
     
     const [isFirstTimeLogin, setFirstTimeLogin] = useState(false);
 
-    const toggleLoginModal = () => {
-        setLoginModalActive(!isLoginModalActive);
-    };
+    const toggleLoginModal = () => setLoginModalActive(!isLoginModalActive);
+    
 
     const toggleForgotPasswordModal = () => {
         setForgotPasswordModalActive(!isForgotPasswordModalActive);
@@ -569,11 +565,12 @@ export const AppContextContainer = ({ children }) => {
     };
     const callAuthService = async (provider, token, input_referalcode) => {
         try {
+        setLoggingIn(true)
         let data;
         defaultDataSettings();
-
         data = await strapi.authenticateProvider(provider, token);
-
+        setLoggingIn(false)
+        
         if (data?.user) {
             window.localStorage.setItem("token", data.jwt);
 
@@ -826,6 +823,7 @@ export const AppContextContainer = ({ children }) => {
         }
         } catch (error) {
             console.log('callauthservice', error)
+            setLoggingIn(false)
             if(error?.message || error?.error?.message){
                 toast({
                     title: error.message || error?.error?.message,
@@ -838,24 +836,20 @@ export const AppContextContainer = ({ children }) => {
         }
     };
   
-    const callCustomAuthService = async (
-        formData,
-        formType,
-        redirectUrl = ""
-    ) => {
+    const callCustomAuthService = async ( formData, formType, redirectUrl = "" ) => {
         let data;
         defaultDataSettings();
+
         if (formType === "signup" || formType === "login") {
+            setLoggingIn(true)
+            const { password, username , email, } = formData
             if (formType === "signup") {
                 try {
-                    const apiValues = {
-                        username: formData.username,
-                        identifier: formData.email,
-                        password: formData.password
-                    };
-                    data = await strapi.register(apiValues);
-                } catch ({ error }) {
-                    if(error.message){
+                    data = await strapi.register({ username , password, identifier: email })
+                    setLoggingIn(false)
+                } catch (error) {
+                    setLoggingIn(false)
+                    if(error?.message){
                         toast({
                             title: error.message,
                             status: "error",
@@ -864,20 +858,14 @@ export const AppContextContainer = ({ children }) => {
                             isClosable: true
                         });
                     }
-                    
-
                     return;
                 }
             }
 
             if (formType === "login") {
                 try {
-                    const apiValues = {
-                        identifier: formData.username,
-                        password: formData.password
-                    };
-                    data = await strapi.login(apiValues);
-                    
+                    data = await strapi.login({ password , identifier: username })
+                    setLoggingIn(false)
                     setJwt(data.jwt);
                     getCurremtLocation().then((res) => {
                         window.localStorage.setItem(
@@ -890,8 +878,9 @@ export const AppContextContainer = ({ children }) => {
                             res?.state
                         );
                     });
-                } catch ({ error }) {
-                    if(error.message){
+                    } catch ( error ) {
+                    setLoggingIn(false)
+                    if(error?.message){
                         toast({
                             title: error.message,
                             status: "error",
@@ -1189,6 +1178,8 @@ export const AppContextContainer = ({ children }) => {
     return (
         <AppContext.Provider
             value={{
+                loggingIn,
+                setLoggingIn,
                 toggleLoginModal,
                 setLoginModalActive,
                 isLoginModalActive,
