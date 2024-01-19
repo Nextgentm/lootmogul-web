082d2d58690152ba6144038966a23b12162e9fba
ssouvikk <souvik.edu.mondal@gmail.com>
2024-01-19 15:56:11 +0530

    err msg showing in toast

--- src/utils/AppContext/index.jsx
+++ src/utils/AppContext/index.jsx
@@ -849,9 +849,10 @@ export const AppContextContainer = ({ children }) => {
                     setLoggingIn(false)
                 } catch (error) {
                     setLoggingIn(false)
-                    if(error?.message){
+                    if(error?.error?.message || error?.message){
+                        console.log(error?.error?.message || error?.message)
                         toast({
-                            title: error.message,
+                            title: error?.error?.message || error?.message,
                             status: "error",
                             duration: 3000,
                             position: "top-right",
@@ -880,9 +881,9 @@ export const AppContextContainer = ({ children }) => {
                     });
                     } catch ( error ) {
                     setLoggingIn(false)
-                    if(error?.message){
+                    if(error?.message || error?.error?.message){
                         toast({
-                            title: error.message,
+                            title: error.message || error?.error?.message,
                             status: "error",
                             duration: 3000,
                             position: "top-right",
