6d6066664e739e304e66cd5e085ad739effde806
vishal-lootmogule <vishal.solanki@nextgentm.com>
2024-03-19 15:09:03 +0530

    Update Logo
    Update Logo

--- src/features/MaintenancePage/index.jsx
+++ src/features/MaintenancePage/index.jsx
@@ -16,7 +16,7 @@ const MaintenancePage = () => {
         <Box width="100vw" pt={["5%", "2%"]} pl={["5%", "2%"]} height="auto">
             
             <Box w={["200px","300px","350px", "350px"]} mt={["5%", "5%","1%","1%"]} ml={["15%", "15%","1%","1%"]}>
-                <Image src="https://lootmogul-wp-cdn-buckets.s3.us-west-2.amazonaws.com/wp-content/uploads/2022/12/20034811/LootMogul-Logo-2.png" alt="Dan Abramov" />
+                <Image src={"assets/Final-Logo-Full.png"} alt="Dan Abramov" />
             </Box>
             <Box  w={["100%","100%","100%","50%"]} p={4} color='white' display={"inline-block"} verticalAlign="top" pt={["5%", "15%"]} pl={["5%", "2%"]}>
                 <Heading variant="siteHeading" color="#fff" fontSize={["52","52","52","85"]} textAlign={['center','center','center','left']}>
