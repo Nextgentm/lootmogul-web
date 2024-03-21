7dbf7e6bf17c04c66520ab9175634f85423ec5ce
vishal-lootmogule <vishal.solanki@nextgentm.com>
2024-03-21 15:10:33 +0530

    Update index.jsx

--- src/components/LMModal/InsufficientFunds/index.jsx
+++ src/components/LMModal/InsufficientFunds/index.jsx
@@ -2,7 +2,7 @@ import { Box, Flex, Heading, Text, Image, Button } from "@chakra-ui/react";
 import { useBreakpointValue } from "@chakra-ui/react";
 import { Pauseicon } from "../../Icons";
 
-const WalletHeader = ({ totalAmount, isDeposit, score }) => {
+const WalletHeader = ({score}) => {
     const currentSize = useBreakpointValue({
         base: "base",
         sm: "sm",
@@ -19,19 +19,19 @@ const WalletHeader = ({ totalAmount, isDeposit, score }) => {
                             paddingTop="5%"
                             borderTopLeftRadius="8px"
                             paddingBottom="0%"
-                            paddingLeft={"2%"}
                             bg="#672099"
                             textAlign="center"
                         >
                             <Heading
                                 mt="10px"
                                 variant="modalHeader"
-                                fontSize={["80px", "80px", "98px"]}
+                                fontSize={["34px", "36px", "52px"]}
                                 fontFamily={"blanch"}
                                 paddingBottom={12}
                                 fontWeight="400"
+                                lineHeight={"0px"}
                             >
-                                Score {score}
+                                Alert! insufficient Chips
                             </Heading>
                         </Box>
                     </Flex>
@@ -40,22 +40,21 @@ const WalletHeader = ({ totalAmount, isDeposit, score }) => {
                 {currentSize === "base" && (
                     <Box
                         width="100%"
-                        paddingTop="5%"
                         borderTopLeftRadius="8px"
                         paddingBottom="0%"
-                        paddingLeft={"2%"}
                         bg="#672099"
                         textAlign="center"
                     >
                         <Heading
                             mt="10px"
                             variant="modalHeader"
-                            fontSize={["80px", "80px", "98px"]}
+                            fontSize={["34px", "36px", "52px"]}
                             fontFamily={"blanch"}
                             paddingBottom={12}
                             fontWeight="400"
+                            lineHeight={"0px"}
                         >
-                            Score {score}
+                            Alert! insufficient Chips
                         </Heading>
                     </Box>
                 )}
@@ -73,29 +72,30 @@ const WalletFooter = () => {
     );
 };
 
-export const GameBody = ({ onJoin, onCancel }) => {
+export const GameBody = () => {
     return (
         <Box m="auto" bg="#fff" pb="10px" width="100%" borderRadius="12px" justifyContent="center">
             <Heading
-                fontSize={[, "24px", "24px", "34px"]}
+                fontSize={[, "14px", "14px", "16px"]}
                 p={"10px"}
                 pt={"25px"}
                 textAlign={'center'}
                 color="#000000"
-                fontWeight="700"
+                fontWeight="400"
                 fontFamily="Open Sans"
             >
-                Your free retry are over
+                It seems you don't have enough chips to continue playing. 
+                Would you like to purchase more chips & play paid games or explore our Free games and enjoy.
             </Heading>
             <Heading
-                fontSize={[, "24px", "24px", "34px"]}
+                fontSize={[, "20px", "20px", "24px"]}
                 padding={"10px"}
                 textAlign={'center'}
                 color="#000000"
                 fontWeight="700"
                 fontFamily="Open Sans"
             >
-                Do you want to <br /> continue?
+                Do you want to add chip?
             </Heading>
             <Button
                 fontSize={['16px', '18px', '22px']}
@@ -104,33 +104,21 @@ export const GameBody = ({ onJoin, onCancel }) => {
                 textAlign={'center'}
                 display="flex"
                 boxShadow={0}
-                onClick={onJoin}
+                //onClick={onJoin}
             >
-                Retry
+                Deposit Now
             </Button>
 
-            <Button
-                fontSize={['10px', '10px']}
-                p={['5px 30px', '5px 20px']}
-                m={["5px auto", "5px auto", "5px auto"]}
-                textAlign={'center'}
-                display="flex"
-                boxShadow={0}
-                height="30px"
-                backgroundImage="linear-gradient(90deg, #672099 0%, #481A7F 100%)"
-                onClick={onCancel}
-            >
-                Cancel
-            </Button>
+            
         </Box>
     );
 };
 
-export const InsufficientFunds = ({ totalAmount, isDeposit, score, onJoin, onCancel }) => {
+export const InsufficientFunds = ({ totalAmount }) => {
     return (
         <Box width="100%" bg="#672099" borderRadius="12px" border="20px solid #672099" boxShadow="0px 6px 40px #090014">
-            <WalletHeader score={score} />
-            <GameBody onCancel={onCancel} onJoin={onJoin} />
+            <WalletHeader score={totalAmount} />
+            <GameBody />
             <WalletFooter />
         </Box>
     );
